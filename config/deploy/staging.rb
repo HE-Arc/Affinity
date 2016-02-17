e7e0b64e7d3bc93d71450021fe161cd9ae088b95
quentin.jeanmonod <quentin.jeanmonod@he-arc.ch>
2016-02-17 00:36:39 -0800

    lol did nothing if gitignore worked

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -7,7 +7,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
-
+server 'srvz-webapp.he-arc.ch', user:'quentin', roles: %w{app db web}
 
 # role-based syntax
 # ==================
