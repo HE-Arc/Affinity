39c543e54b5ae65ecd89de25aeba1edac71b0036
Gabriel Freitas <frg.gabriel.freitas@gmail.com>
2016-02-17 14:58:16 +0100

    TRUE QUOTES

--- config/deploy.rb
+++ config/deploy.rb
@@ -49,7 +49,7 @@ namespace :deploy do
   after :finished, :restart_puma do
     on roles(:web) do
       sudo 'sv restart puma'
-      sudo 'rm -r /tmp/#{fetch(:application)}'
+      sudo "rm -r /tmp/#{fetch(:application)}"
 
       #execute :sudo, 'sv restart puma'
     end
