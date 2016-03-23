1dd2873dac808bad089f139ffedf2d686ab275ac
Gabriel Freitas <frg.gabriel.freitas@gmail.com>
2016-03-23 08:32:03 +0100

    Bad commit ; too much stuff done at once

--- app/controllers/heroes_controller.rb
+++ app/controllers/heroes_controller.rb
@@ -8,14 +8,20 @@ class HeroesController < ApplicationController
     @hero = Hero.find(params[:id])
 
     #Get 5 good affinites heroes
-    @goods = Relation.where(:hero_left == params[:id]).order("score desc").limit(5)
+    @goods = Relation.where(hero_left: params[:id]).order("score desc").limit(5)
 
     @good_heroes = []
     @goods.each do |good|
-      @good_heroes.append(Hero.find(good.hero_left))
+      @good_heroes.append(Hero.find(good.hero_right))
     end
 
     #Get 5 bad affinities heroes
+    @bads = Relation.where(hero_left: params[:id]).order("score asc").limit(5)
+
+    @bad_heroes = []
+    @bads.each do |good|
+      @bad_heroes.append(Hero.find(good.hero_right))
+    end
   end
 
   def new
@@ -35,5 +41,4 @@ class HeroesController < ApplicationController
   def hero_params
     params.require(:hero).permit(:name).permit(:display_name).permit(:uri)
   end
-
 end
