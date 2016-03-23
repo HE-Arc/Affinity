6e0e168b2b57454a93be41d67ac4a05b2ac0decc
Gabriel Freitas <frg.gabriel.freitas@gmail.com>
2016-03-23 11:29:59 +0100

    Improved N+1 request in relation

--- app/controllers/heroes_controller.rb
+++ app/controllers/heroes_controller.rb
@@ -1,27 +1,13 @@
 class HeroesController < ApplicationController
 
   def index
-    @heroes = Hero.all   
+    @heroes = Hero.all
   end
-  
+
   def information
     @hero = Hero.find(params[:id])
-
-    #Get 5 good affinites heroes
-    @goods = Relation.where(hero_left: params[:id]).order("score desc").limit(5)
-
-    @good_heroes = []
-    @goods.each do |good|
-      @good_heroes.append(Hero.find(good.hero_right))
-    end
-
-    #Get 5 bad affinities heroes
-    @bads = Relation.where(hero_left: params[:id]).order("score asc").limit(5)
-
-    @bad_heroes = []
-    @bads.each do |good|
-      @bad_heroes.append(Hero.find(good.hero_right))
-    end
+    @good_heroes = Relation.best(@hero.id)
+    @bad_heroes = Relation.worst(@hero.id)
   end
 
   def new
--- app/models/relation.rb
+++ app/models/relation.rb
@@ -2,4 +2,12 @@ class Relation < ActiveRecord::Base
   #Validates uniqueness of pairs Left-Right
   validates_uniqueness_of :hero_left, :scope => [:hero_right]
   validates_uniqueness_of :hero_right, :scope => [:hero_left]
+
+  scope :best, ->(hero_id) {
+    Hero.find(where(hero_left: hero_id).order("score desc").limit(5).pluck(:hero_right))
+  }
+
+  scope :worst, ->(hero_id) {
+    Hero.find(where(hero_left: hero_id).order("score asc").limit(5).pluck(:hero_right))
+  }
 end
